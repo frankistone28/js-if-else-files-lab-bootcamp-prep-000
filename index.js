3d50545990fadf62b2ddfdd0961c34198265ec59
Franklyn Thompson <frankistone28@outlook.com>
2017-09-14 14:43:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 var name = "Susan"
 var height = parseInt("70")
 
-if(name === "Susan"){
-  console.log("You got the name");
-}
-
-else{
-  console.log("Expected the name to be Susan");
-}
+// if(name === "Susan"){
+//   console.log("You got the name");
+// }
+// 
+// else{
+//   console.log("Expected the name to be Susan");
+// }
 // Don't worry about this
 module.exports = { name, height
 }
